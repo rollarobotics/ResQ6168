1926f60f68a3b5f7e458ba6e4e7d23a7b330b6ff
spmcenaney <spmcenaney@outlook.com>
2016-02-10 13:23:47 -0600

    Update RobotTelemetry.java

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/RobotTelemetry.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/RobotTelemetry.java
@@ -4,5 +4,13 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 public class RobotTelemetry extends RobotHardware {
-
+  
+  public RobotTelemetry {
+    
+  }
+  
+  public void init {
+    telemetry.addData("1" , "Init Hook Position" + getPosition(hook));
+    telemetry.addData("2" , "Init Spinner Position" + getPosition(spinner));
+  }
 }
